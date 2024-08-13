1f9270460c371e403c4493cae7092babb57b181c
Minh Lai Huynh Nhat <mlaihuynhnhat@gmail.com>
2024-08-13 21:41:39 +0700

    Init Example

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+      <p className="m-4 font-semibold">Hello Auth</p>
+      <p className="m-4 font-semibold">HELLO NEXTJS</p>
+    </div>
+  );
+}
