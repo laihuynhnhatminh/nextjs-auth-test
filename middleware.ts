cf40770ad0b2384442e25bfd7a1f5d132c238e84
Minh Lai Huynh Nhat <nhatminhwork1996@gmail.com>
2024-09-03 16:19:44 +0700

    feat: Test

--- middleware.ts
+++ middleware.ts
@@ -8,27 +8,27 @@ import {
 } from '@/routes/routes';
 
 export default auth((req) => {
-  const { nextUrl } = req;
-  const isLoggedIn = !!req.auth;
+  // const { nextUrl } = req;
+  // const isLoggedIn = !!req.auth;
 
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  // const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  // const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  // const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  if (isApiAuthRoute) {
-    return;
-  }
+  // if (isApiAuthRoute) {
+  //   return;
+  // }
 
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-    }
-    return;
-  }
+  // if (isAuthRoute) {
+  //   if (isLoggedIn) {
+  //     return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+  //   }
+  //   return;
+  // }
 
-  if (!isLoggedIn && !isPublicRoute) {
-    return Response.redirect(new URL(DEFAULT_UNAUTH_REDIRECT, nextUrl));
-  }
+  // if (!isLoggedIn && !isPublicRoute) {
+  //   return Response.redirect(new URL(DEFAULT_UNAUTH_REDIRECT, nextUrl));
+  // }
 
   return;
 });
